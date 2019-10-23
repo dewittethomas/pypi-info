35bbe59c06acbdb71ecf33ba11a7dd8a67589a06
Thomas Dewitte <thomasdewittecontact@gmail.com>
2019-10-23 21:40:37 +0200

    .gitignored some things

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const fetch = require('node-fetch');
-
-module.exports = function getPackage(package) {
-    const res = await fetch(`https://pypi.org/pypi/${package}/json`);
-    return res.json().info;
+module.exports = async function getPackage(package) {
+  const res = await fetch(`https://pypi.org/pypi/${package}/json`);
+  return res.json().info;
 };
